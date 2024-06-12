07604bdcee90f92aade4b0b2eb788bb15fc02d8a
Gabriela <gabrielasiqueiracaputti@gmail.com>
2024-06-12 16:24:05 -0300

    perfil pré finalizado

--- Atividade/js/script.js
+++ Atividade/js/script.js
@@ -10,7 +10,7 @@ const isLight = document.documentElement.classList.contains("light");
 
 const imagem = isLight ? "./img/gabicolorida.jpg" : "./img/gabipretoe.jpg";
 
-const trocaalt = isLight ? "./img/gabicolorida.jpg" : "./img/gabipretoe.jpg";
+const trocaalt = isLight ? "Selfie tirada por uma menina com o cabelo castanho escuro ondulado, regata verde, colar dourado, com o rosto inclinado, na frente de um fundo amadeirado." : "Selfie em preto e branco tirada por uma menina com o cabelo ondulado, regata, colar, com o rosto inclinado, na frente de um fundo amadeirado.";
 
 document.querySelector("#perfil img").setAttribute("src", imagem);
 document.querySelector("#perfil img").setAttribute("alt", trocaalt);
